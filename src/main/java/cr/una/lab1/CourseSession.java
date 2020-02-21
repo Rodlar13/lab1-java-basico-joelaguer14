c5359b223a56ddfbd135f08122a550a05281042c
joelaguer14 <46728129+joelaguer14@users.noreply.github.com>
2020-02-21 11:59:57 -0600

    Everything completed, just the 'getEndDate()' missing...

--- src/main/java/cr/una/lab1/CourseSession.java
+++ src/main/java/cr/una/lab1/CourseSession.java
@@ -9,15 +9,108 @@ import java.util.Date;
  */
 public class CourseSession {
 
+    /**
+     *
+     */
     private String department;
+    /**
+     *
+     */
     private String code;
+    /**
+     *
+     */
     private Date startDate;
+    /**
+     *
+     */
     private ArrayList<Student> students;
+
+    /**
+     *
+     * @param department
+     * @param code
+     * @param startDate
+     */
+    CourseSession(String department, String code, Date startDate){
+        this.department=department;
+        this.code=code;
+        this.startDate=startDate;
+        students=new ArrayList<>();
+    }
+
     /**
     * @param student
     */
     public void enrollStudent(Student student){
 
     }
+    public void getEndDate(){
+        Calendar calendar=Calendar.getInstance();
+        calendar.set(startDate.);
+    }
 
+    /**
+     *
+     * @param department
+     */
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getDepartment() {
+        return department;
+    }
+
+    /**
+     *
+     * @param code
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     *
+     * @param startDate
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int getNumberOfStudents(){
+        return students.size();
+    }
 }
--- src/main/java/cr/una/lab1/Student.java
+++ src/main/java/cr/una/lab1/Student.java
@@ -5,4 +5,48 @@ package cr.una.lab1;
  */
 public class Student {
 
+    private String name;
+    private String lastName;
+
+    /**
+     *
+     * @param name
+     * @param lastName
+     */
+    Student(String name, String lastName){
+        this.name=name;
+        this.lastName=lastName;
+    }
+
+    /**
+     *
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @param lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getLastName() {
+        return lastName;
+    }
 }
